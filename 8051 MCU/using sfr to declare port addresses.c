a2de03c991995997c9ed6db21a9be761aa4f32be
Eshaan Matthew Kuttikad <85567814+EK232005@users.noreply.github.com>
2024-09-28 00:59:12 +0530

    Update using sfr to declare port addresses.c

--- 8051 MCU/using sfr to declare port addresses.c
+++ 8051 MCU/using sfr to declare port addresses.c
@@ -1,10 +1,10 @@
-#include <reg51.h>
+//doesnt require any header file since we have defined the ports outselves
 
 sfr P0 = 0x80; //port addresses
 sfr P1 = 0x90;
 sfr P2 = 0xA0;
 
-void delay(unsigned int itime){
+void Delay(unsigned int itime){
 	unsigned int i,j;
 	for(i=0;i<itime;i++){
 		for(j=0;j<1275;j++);
@@ -15,10 +15,10 @@ void main(){
 	while(1){
 		P0 = 0x55;
 		P2 = 0x55;
-		delay(250);
+		Delay(250);
 		P0 = 0xAA;
 		P2 = 0xAA;
-		delay(250);
+		Delay(250);
 	}
 }
 
