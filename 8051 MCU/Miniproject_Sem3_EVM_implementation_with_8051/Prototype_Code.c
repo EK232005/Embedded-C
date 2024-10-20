0b73cc9e86ad087d767051a76413af86776bce38
Eshaan Matthew Kuttikad <85567814+EK232005@users.noreply.github.com>
2024-10-20 16:52:22 +0530

    Update Prototype_Code.c

--- 8051 MCU/Miniproject_Sem3_EVM_implementation_with_8051/Prototype_Code.c
+++ 8051 MCU/Miniproject_Sem3_EVM_implementation_with_8051/Prototype_Code.c
@@ -13,23 +13,23 @@ void voting_mode();
 //LCD FUNCTIONS and CONSTANTS- Send Data and Commands along with Initialization
 
 #define display_port P2 //Data pins connected to port 2 on microcontroller
-sbit rs = P3^2;  //RS pin connected to pin 2 of port 3
-sbit rw = P3^3;  // RW pin connected to pin 3 of port 3
-sbit e =  P3^4;  //E pin connected to pin 4 of port 3
+sbit rs = P3^2;
+sbit rw = P3^3;
+sbit e =  P3^4;
 
 void lcd_init(){ //Initialize lcd function
-	lcd_cmd(0x38);  // for using 2 lines and 5X7 matrix of LCD
+	lcd_cmd(0x38);              // for using 2 lines and 5X7 matrix of LCD
   msdelay(10);
-  lcd_cmd(0x0F);  // turn display ON, cursor blinking
+  lcd_cmd(0x0F);              // turn display ON, cursor blinking
   msdelay(10);
-	lcd_cmd(0x01);  //clear screen
+	lcd_cmd(0x01);              //clear screen
   msdelay(10);
-  lcd_cmd(0x81);  // bring cursor to position 1 of line 1
+  lcd_cmd(0x81);              // bring cursor to position 1 of line 1
   msdelay(10);
 }
 void lcd_cmd(unsigned char command){  //Function to send command instruction to LCD
 	display_port = command;
-  rs= 0;                   //send command mode
+  rs= 0;                      //send command mode
   rw=0;
   e=1;
   msdelay(1);
@@ -37,20 +37,28 @@ void lcd_cmd(unsigned char command){  //Function to send command instruction to 
 }
 void lcd_data(unsigned char disp_data){  //Function to send display data to LCD
 	display_port = disp_data;
-  rs= 1;                   //send data mode
+  rs= 1;                      //send data mode
   rw=0;
   e=1;
   msdelay(1);
   e=0;
 }
 void send_msg(unsigned char *a){
-  int l=0;
-  lcd_init();
-  while(*a != '\0') // searching the null terminator in the sentence
-	{
-	 lcd_data(*a);
-   a++;
-   msdelay(5);
+  unsigned int l=0;
+	unsigned char char_len=0;
+  lcd_cmd(0x01);     		      //clear screen
+	lcd_cmd(0x80);     		      //force cursor back to start of first line 
+  while(*a != '\0'){ 		      // searching the null terminator in the sentence
+		lcd_data(*a);
+		a++;
+		char_len++;
+		if(char_len==16){         //if message is greater than 16, go to 2nd line
+			lcd_cmd(0xC0);
+		}
+		msdelay(5);
+		if(char_len==32){         //message is greater than lcd can display
+			break;
+		}
 	}
 }
 //KEYPAD FUNCTIONS and CONSTANTS
@@ -70,35 +78,35 @@ unsigned char keypad[4][4] = {
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}
 };
-
 char keypad_scan() {
 	row1 = 0; row2 = 1; row3 = 1; row4 = 1;  // Activate row 1
-  if (col1 == 0) { delay(); while(col1==0); return keypad[0][0]; }
-  if (col2 == 0) { delay(); while(col2==0); return keypad[0][1]; }
-  if (col3 == 0) { delay(); while(col3==0); return keypad[0][2]; }
-  if (col4 == 0) { delay(); while(col4==0); return keypad[0][3]; }
+  if (col1 == 0) { ; while(col1==0); return keypad[0][0]; }
+  if (col2 == 0) { msdelay(10); while(col2==0); return keypad[0][1]; }
+  if (col3 == 0) { msdelay(10); while(col3==0); return keypad[0][2]; }
+  if (col4 == 0) { msdelay(10); while(col4==0); return keypad[0][3]; }
 
   row1 = 1; row2 = 0; row3 = 1; row4 = 1;  // Activate row 2
-  if (col1 == 0) { delay(); while(col1==0); return keypad[1][0]; }
-  if (col2 == 0) { delay(); while(col2==0); return keypad[1][1]; }
-  if (col3 == 0) { delay(); while(col3==0); return keypad[1][2]; }
-  if (col4 == 0) { delay(); while(col4==0); return keypad[1][3]; }
+  if (col1 == 0) { msdelay(10); while(col1==0); return keypad[1][0]; }
+  if (col2 == 0) { msdelay(10); while(col2==0); return keypad[1][1]; }
+  if (col3 == 0) { msdelay(10); while(col3==0); return keypad[1][2]; }
+  if (col4 == 0) { msdelay(10); while(col4==0); return keypad[1][3]; }
 
   row1 = 1; row2 = 1; row3 = 0; row4 = 1;  // Activate row 3
-  if (col1 == 0) { delay(); while(col1==0); return keypad[2][0]; }
-  if (col2 == 0) { delay(); while(col2==0); return keypad[2][1]; }
-  if (col3 == 0) { delay(); while(col3==0); return keypad[2][2]; }
-  if (col4 == 0) { delay(); while(col4==0); return keypad[2][3]; }
+  if (col1 == 0) { msdelay(10); while(col1==0); return keypad[2][0]; }
+  if (col2 == 0) { msdelay(10); while(col2==0); return keypad[2][1]; }
+  if (col3 == 0) { msdelay(10); while(col3==0); return keypad[2][2]; }
+  if (col4 == 0) { msdelay(10); while(col4==0); return keypad[2][3]; }
 
   row1 = 1; row2 = 1; row3 = 1; row4 = 0;  // Activate row 4
-  if (col1 == 0) { delay(); while(col1==0); return keypad[3][0]; }
-  if (col2 == 0) { delay(); while(col2==0); return keypad[3][1]; }
-  if (col3 == 0) { delay(); while(col3==0); return keypad[3][2]; }
-  if (col4 == 0) { delay(); while(col4==0); return keypad[3][3]; }
+  if (col1 == 0) { msdelay(10); while(col1==0); return keypad[3][0]; }
+  if (col2 == 0) { msdelay(10); while(col2==0); return keypad[3][1]; }
+  if (col3 == 0) { msdelay(10); while(col3==0); return keypad[3][2]; }
+  if (col4 == 0) { msdelay(10); while(col4==0); return keypad[3][3]; }
 
   return 0;  // No key pressed
 }
 //LOGIC MODES - FUNCTIONS and CONSTANTS
+
 unsigned int candidate_votes[4]; //stores individual candidate votes in the array (upto 4 candidates)
 
 void msdelay(unsigned int ms){ //Function to create delay
@@ -114,25 +122,59 @@ void msdelay(unsigned int ms){ //Function to create delay
 	}
 }
 void modechoose(){	//called on power on, and after each time user finishes voting
+	unsigned char a[15] = "WELCOME TO EVM";
+	unsigned char b[31] = "PRESS # TO SHOW VOTING RESULTS";
+	unsigned char c[12] = "VOTING MODE";
+	unsigned char d[12] = "RESULT MODE";
+	send_msg(a);
+	msdelay(2000);
+	send_msg(b);
 	while(1){
 		unsigned char temp = keypad_scan();
 		if(temp != 0){
-			if(temp != keypad[3][2]){
-				voting_mode();
+			if(temp == keypad[3][2]){
+				send_msg(c);
+				msdelay(2000);
+				result_mode();
 				break;
 			}
 			else{
-				result_mode();
+				send_msg(d);
+				msdelay(2000);
+				voting_mode();
 				break;
 			}
 		}
 	}
 }
-
 void voting_mode(){
 	//take valid user vote and increment party counter.
+	unsigned char x[33] = "PARTY1   PARTY2  PARTY3   PARTY4";
+	unsigned char y[] = "CONFIRM VOTE ?     (A - YES)";
+	unsigned char invalid[] = "INVALID INPUT     PLS TRY AGAIN"	;
+	unsigned char z[] = "THANK YOU FOR        VOTING";
+	unsigned char input;
+	while(1){
+		send_msg(x);
+		msdelay(5000);
+		input = keypad_scan();		
+		if(keypad_scan!=0){
+			if(input == keypad[0][0] || input == keypad[0][1] || input == keypad[0][2] || input == keypad[0][3] || keypad[1][0]){
+				send_msg(y);
+				msdelay(5000);
+				if(keypad_scan() != keypad[0][3]){
+					continue;
+				}
+				else{
+					send_msg(z);
+					msdelay(10000);
+					break;
+				}
+			}
+		}
+	}
 }
 
 void main(){
-	
+	send_msg("HELLO");
 }
